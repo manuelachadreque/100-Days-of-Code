9588231eb114b7b4dd5bc830887d9a8735ccccc6
Manuela da Cruz Chadreque <manuelachadreque@gmail.com>
2021-09-14 18:52:59 +0000

    BMI nd Your Life in Weeks

---
+++ BMI.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+years_left=90-int(age)
+days=int(years_left)*365
+weeks=int(years_left)*52
+months=int(years_left)*12
+#Write your code below this line 👇
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
